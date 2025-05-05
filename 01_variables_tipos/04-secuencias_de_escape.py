658c0ed9afb33a5a8e5e3bc10d6b1fd48223c42e
Oscar <oscarfranciscomaciel@gmail.com>
2025-05-05 11:18:08 -0300

    Secuencias de Escape

---
+++ 01_variables_tipos/04-secuencias_de_escape.py
@@ -0,0 +1,20 @@
+# SECUENCIAS DE ESCAPE
+#Las secuencias de escape en Python se usan para representar caracteres especiales dentro de strings, como saltos de línea, tabulaciones, comillas dentro de comillas, etc.
+
+# Salto de línea \n
+print("Hola\nMundo")                #Hola
+                                    #Mundo
+
+# Tabulación \t
+print("Hola\tMundo")                #Hola   Mundo
+
+# Barra invertida literal \
+print("Esto es una barra invertida: \\")        #Esto es una barra invertida: \
+
+# Comilla simple dentro de comillas simples (usando escape)   \  
+print('Es una contraccion: It\'s fine')         #Es una contraccion: It's fine
+
+# Comilla doble dentro de comillas dobles (usando escape) \...\
+print("Ella dijo: \"Hola\"")                    #Ella dijo: "Hola"
+
+
